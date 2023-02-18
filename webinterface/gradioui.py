b6b79330217e63e53801ac9acdbe387c03c6fc7e
dino1729 <dino.feynman@gmail.com>
2023-02-18 15:42:33 -0800

    Added new interface

--- webinterface/gradioui.py
+++ webinterface/gradioui.py
@@ -2,6 +2,7 @@ import os
 from urllib.parse import parse_qs, urlparse
 
 import gradio as gr
+from IPython.display import Markdown, display
 from langchain import OpenAI
 from langchain.agents import initialize_agent
 from pytube import YouTube
@@ -20,6 +21,7 @@ ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp4', 'avi', 'mkv', '
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
@@ -33,9 +35,11 @@ def upload_file(file):
         message = 'Allowed file types are pdf, png, jpg, jpeg, gif, mp4, avi, mkv, mov'
         return message
 
-def download_ytvideo(yturl):
-    if yturl:
-        yt = YouTube(yturl)
+
+def download_ytvideo(url):
+    #If there is a url in the input field, download the video
+    if url:
+        yt = YouTube(url)
         query_params = parse_qs(urlparse(yt).query)
         video_name = query_params['v'][0]
         video_name_display = f'<h2>Video Name: {video_name}</h2>'
@@ -44,33 +48,33 @@ def download_ytvideo(yturl):
         return message, video_name_display
     else:
         message = 'Please enter a valid Youtube URL'
-        return message, ""
+        return message, ''
 
 def build_index():
     documents = SimpleDirectoryReader(UPLOAD_FOLDER).load_data()
     index = GPTSimpleVectorIndex(documents)
-    index.save_to_disk("data/index.json")
+    index.save_to_disk(os.path.join(UPLOAD_FOLDER, "index.json"))
     message = 'Index built successfully! You can now ask questions!!'
     return message
 
+
 def ask(question):
-    index = GPTSimpleVectorIndex.load_from_disk("data/index.json")
+    index = GPTSimpleVectorIndex.load_from_disk(os.path.join(UPLOAD_FOLDER, "index.json"))
     response = index.query(question)
     return response
 
-gradio_interface = gr.Interface(
-    fn=ask,
-    inputs=gr.inputs.Textbox(label="Question"),
-    outputs=gr.outputs.Textbox(label="Answer"),
-    title="GPT Index Q&A",
-    description="Ask any question and GPT will find the closest matching answer from the indexed data.",
-    examples=[
-        ['How to upload an image?'],
-        ['What file formats are allowed?'],
-        ['What is the purpose of this tool?']
-    ],
-    live=True,
-)
+# Set up Gradio interface
+file_uploader = gr.inputs.File(upload_folder=UPLOAD_FOLDER)
+text_input = gr.inputs.Textbox(lines=3, placeholder="Type your question here...")
+url_input = gr.inputs.Textbox(label="Youtube URL")
+output_text = gr.outputs.Textbox(label="Answer", default="No answer yet.")
+interface = gr.Interface(upload_file, [file_uploader], output_text, capture_session=True, title="File Uploader")
 
-if __name__ == '__main__':
-    gradio_interface.launch()
+interface2 = gr.Interface(download_ytvideo, [url_input], [output_text, gr.outputs.HTML(label="Video Name")], capture_session=True, title="Youtube Video Downloader")
+
+interface3 = gr.Interface(build_index, [], output_text, capture_session=True, title="Build Index")
+
+interface4 = gr.Interface(ask, text_input, output_text, capture_session=True, title="Ask a Question")
+
+# Launch the app
+gr.Interface([interface, interface2, interface3, interface4], gradio_server_name="my-gradio-app").launch()
